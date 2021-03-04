9b4874138e26878227f51dc8bf79870d056495e4
jude-co <79138123+jude-co@users.noreply.github.com>
2021-03-04 09:09:08 +0100

    Add files via upload

---
+++ Documents/js.js
@@ -0,0 +1,13 @@
+document.getElementById('submit').addEventListener('click', function(e){
+    e.preventDefault();
+var x= document.getElementById('First').value
+var y= document.getElementById('Last').value
+var a= document.getElementById('Age').value
+
+//var birthDate= new Date(a);
+//var diff= Date.now()-birthDate.getTime();
+//var age= new Date(diff);
+//var realAge= Math.abs(age.getUTCFullYear()- 1970);
+var birth= new Date(a);
+var diff= Date.now()-birth.getTime();
+var Real= new Date(diff);
